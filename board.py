c70cd85991740d0bb5b2b4e6da21c69f321da36f
Andy Zhang <andizzle.zhang@gmail.com>
2014-03-01 01:15:08 +1100

    Update board class, flip now accept move obj.

--- board.py
+++ board.py
@@ -1,13 +1,17 @@
+from Move import move
+
+
 class Board:
 
-    grid = []
     size = 0
+    moves = 0
 
     def __init__(self, board_size):
         """
         Construct a board grid like
         """
         x = 0
+        self.grid = []
         self.size = board_size
         while(x < board_size):
             row = [1 for size in range(board_size)]
@@ -21,22 +25,24 @@ class Board:
         print '-' * len(self.grid) * 3
         for x in self.grid:
             print x
+        print '-' * len(self.grid) * 3
 
-    def flip_row(self, row, start, cycle):
-        """
-        Flip a single row
-        """
-        while(cycle >= 0):
-            self.grid[row][cycle] = 1 if self.grid[row][cycle] == 0 else 0
-            cycle = cycle - 1
-
-    def flip(self, upper_right, width, height):
+    def flip(self, move):
         """
         Flip the board within the range
-        e.g, upper_right = (1, 4)
+        e.g, upper_right = (3, 0)
         """
+        upper_right = move.upper_right
+        width = move.width
+        height = move.height
+        self.moves = self.moves + 1
         x = upper_right[0]
-        y = upper_right[1]
-        while(x - height > 0):
-            self.flip_row(height - y, width, width - y)
-            height = height + 1
+        while(width > 0):
+            y = upper_right[1]
+            width = width - 1
+            loop_height = height
+            while(loop_height > 0):
+                loop_height = loop_height - 1
+                self.grid[y][x] = 1 if self.grid[y][x] == 0 else 0
+                y = y + 1
+            x = x - 1
